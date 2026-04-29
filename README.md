ce117e0d17e86684d77f96c85115d1ff90ccaca9
siyavuyachagi <syavuya08@gmail.com>
2026-04-29 03:54:26 +0200

    docs(readme): logo image

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# TypeSharp
-
 <p align="center">
-  <img src="https://raw.githubusercontent.com/siyavuyachagi/typesharp/main/public/images/banner-transparent.png" alt="TypeSharp" height="120" />
+  <img src="https://raw.githubusercontent.com/siyavuyachagi/typesharp/main/public/BannerTransparent.svg" alt="TypeSharp" height="120" />
 </p>
 
 <p align='center'>
--- tests/fixtures/csharp/obj/Debug/net8.0/TypeSharpTest.AssemblyInfo.cs
+++ tests/fixtures/csharp/obj/Debug/net8.0/TypeSharpTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypeSharpTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25dcb8b38152b86238c1fc1c98d8e1192813b78b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6d0a447005f67cb4734807ffde98b7e7ae085c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypeSharpTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypeSharpTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
